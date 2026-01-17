36d81201584a8416fd48784bee95fb1afdbcfba0
Xiaoxuan Liu <liu.xiaoxu@northeastern.edu>
2026-01-17 17:32:39 -0500

    required

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the specific button element
+const myButton = document.getElementById('alertButton');
+
+// Add a click event listener to that specific element
+myButton.addEventListener('click', function() {
+    // Trigger the alert dialog
+    alert('Hello! Thank you for visiting my Project 0 website.');
+});
